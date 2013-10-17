7410de27f53c8575db9f3f7fee2eac4ac4d4d548
Zsolt Molnar <zsolt@zsoltmolnar.hu>
2013-10-17 11:49:35 +0200

    Initial commit of APIXServer

---
+++ env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/molnarzs/Dropbox/Projects/2013/APIXMobil/APIXServer/env/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
