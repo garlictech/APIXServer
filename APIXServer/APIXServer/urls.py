66a5795e24be964a2da2a66a09abadae0aff2acc
Zsolt Molnar <zsolt@zsoltmolnar.hu>
2013-10-18 12:21:32 +0200

    - fabric installed. - Hello, world app installed.

--- APIXServer/APIXServer/urls.py
+++ APIXServer/APIXServer/urls.py
@@ -5,8 +5,7 @@ from django.conf.urls import patterns, include, url
 # admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'APIXServer.views.home', name='home'),
+    url(r'^$', 'server.views.index', name='index'),
     # url(r'^APIXServer/', include('APIXServer.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
---
+++ APIXServer/fabfile.py
@@ -0,0 +1,12 @@
+from fabric.api import local
+
+
+def prepare_deployment(branch_name):
+    local('python manage.py test server')
+    local('git add -p && git commit')
+    #local('git checkout master && git merge ' + branch_name)
+
+
+def runserver(branch_name):
+    local('python manage.py runserver')
+
