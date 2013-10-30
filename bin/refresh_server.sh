f742a8986b20a1b2b52cb8a72c3dea2d47ed5710
Zsolt Molnar <zsolt@zsoltmolnar.hu>
2013-10-30 13:42:24 +0100

    Icons added as static files

--- bin/refresh_server.sh
+++ bin/refresh_server.sh
@@ -1,6 +1,7 @@
 ROOT=/home/apixserver/public_html/APIXServer/APIXServer
 cd $ROOT
 python manage.py syncdb --noinput
+python manage.py collectstatic
 pushd APIXServer
 ln -sf settings_prod.py settings_actual.py
 popd
